aac38b4d6462b8cdf2d696e97d1f9c90eda1b444
alena-yudzina <lenok.ok.lol@gmail.com>
2022-01-28 12:55:54 +0300

    add getting addresses and menu

---
+++ pizza.py
@@ -0,0 +1,8 @@
+import requests
+
+
+address_urls = 'https://dvmn.org/media/filer_public/90/90/9090ecbf-249f-42c7-8635-a96985268b88/addresses.json'
+menu_urls = 'https://dvmn.org/media/filer_public/a2/5a/a25a7cbd-541c-4caf-9bf9-70dcdf4a592e/menu.json'
+
+addresses = requests.get(address_urls)
+menues = requests.get(menu_urls)
